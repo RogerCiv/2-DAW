2396ad1d254ebda5b7c13abca4581572d31c8383
RogerCiv <rogeliocivantos@gmail.com>
2023-11-01 16:24:28 +0100

    update

--- DWEC/Examen/examen_carrito_libros/main.js
+++ DWEC/Examen/examen_carrito_libros/main.js
@@ -5,16 +5,17 @@ import { buscarLibro, menorStock, libroMasVendido } from "./funcionalidades";
 const carrito = new Carrito();
 
 
-
-const libro = new Libro("978-0134685991","Effective Java",368,"Joshua Bloch","Addison-Wesley",["Programación, Java"],45.99,"★★★★★",10);
-
 carrito.agregarProducto("978-0134685991",2)
 carrito.agregarProducto("978-0135471944",3)
+carrito.agregarProducto("978-1449337711",2)
+carrito.agregarProducto("978-1492040736",5)
+
+carrito.agregarDescuento()
 
 //carrito.eliminarProducto("978-0134685991")
 
 console.log(carrito.calcularTotalCarrito());
-console.log(libro)
+
 
 console.log(carrito)
 console.log(buscarLibro("Addison-Wesley"))
